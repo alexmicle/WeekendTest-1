713ecfb123e9fb2ebe63c78c31a94715b8873c27
alexm <alexmicle@gmail.com>
2017-02-25 12:33:00 +0200

    --moved function to calc class

--- src/Calc.java
+++ src/Calc.java
@@ -3,7 +3,34 @@
  * @since 25/02/2017.
  */
 public class Calc {
-    public static int add(int cucurigu , int b) {
+    public static int add(int cucurigu, int b) {
         return cucurigu + b;
     }
+
+
+    public static int add2(int x, int y) {
+
+        int max, min;
+        if (x > y) {
+            max = x;
+            min = y;
+        } else {
+            max = y;
+            min = x;
+        }
+        System.out.println(max + "|" + min);
+        int sum = max;
+        /*while(y!=0){
+            sum +=1;
+            y-=1;
+        }
+        */
+        for (int i = 1; i <= min; i++) {
+            sum += 1;
+            SkeletonJava.printConsole("Adding 1 to sum");
+        }
+
+        return sum;
+    }
+
 }
--- src/Example.java
+++ src/Example.java
@@ -12,30 +12,8 @@ public class Example {
         int x = SkeletonJava.readIntConsole("x=");
         int y = SkeletonJava.readIntConsole("y=");
 
-
-        int max,min;
-        if(x>y) {
-            max = x;
-            min = y;
-        }
-        else{
-            max =y;
-            min = x;
-        }
-        System.out.println(max + "|" + min);
-        int sum = max ;
-        /*while(y!=0){
-            sum +=1;
-            y-=1;
-        }
-        */
-        for(int i = 1; i<=min; i++)
-        {
-            sum +=1;
-            SkeletonJava.printConsole("Adding 1 to sum");
-        }
-
-        SkeletonJava.printConsole(max + "+" + min + "=" + sum);
+        int sum = Calc.add2(x, y);
+        SkeletonJava.printConsole(x + "+" + y + "=" + sum);
     }
 
 }
