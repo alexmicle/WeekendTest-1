3700e89329b736ef024802e552df4bf24bb12f66
alexm <alexmicle@gmail.com>
2017-02-25 13:19:21 +0200

    --convert to binary class

--- src/Conversion.java
+++ src/Conversion.java
@@ -4,5 +4,30 @@
 public class Conversion {
 
 
+    public static String conv(int x){
+
+        String a = "";
+
+        int d= x;
+        while(d!=0){
+
+            int c = d/2;
+            int r = d%2;
+            d = c;
+            a = a.concat(Integer.toString(r));
+        }
+
+        a = new StringBuilder(a).reverse().toString();
+
+        return a;
+    }
+
+    public static double fToc(double x){
+
+        double c = (5/9) * (x - 32);
+
+        return c;
+    }
+
 
 }
--- src/Example.java
+++ src/Example.java
@@ -9,15 +9,18 @@ public class Example {
         int suma = Calc.add(2, 3);
         System.out.println(suma);  //5
 
-        int x = SkeletonJava.readIntConsole("x=");
-        int y = SkeletonJava.readIntConsole("y=");
+        double x = SkeletonJava.readIntConsole("x=");
+        //int y = SkeletonJava.readIntConsole("y=");
 
         //int sum = Calc.add2(x, y);
         //int dif = Substract.sub(x,y);
-        int prod = Multy.prod(x,y);
+        //int prod = Multy.prod(x,y);
         //SkeletonJava.printConsole(x + "+" + y + "=" + sum);
         //SkeletonJava.printConsole(x + "-" + y + "=" + dif);
-        SkeletonJava.printConsole(x + "*" + y + "=" + prod);
+        //SkeletonJava.printConsole(x + "*" + y + "=" + prod);
+
+        //SkeletonJava.printConsole(Conversion.conv(x));
+        SkeletonJava.printConsole(Conversion.fToc(x));
     }
 
 }
